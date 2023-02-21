2f84999f7567d62ad145d59eacc73c6ecd12c2ca
ajegorovs <aleksandrs.jegorovs@lu.lv>
2023-02-21 19:20:19 +0200

    quick edit for visualisation. test.py has a base code for annotaion placement generation. next: implement into main code. maybe remove nearest line distance search for simple min/max. but  computation wise it should be very similar- one run though controur.

--- test.py
+++ test.py
@@ -586,32 +586,119 @@ def extrapolate(data, maxInterpSteps = 3, maxInterpOrder = 2, smoothingScale = 0
 
 
 #print(np.argwhere(arr == 22).reshape(-1).tolist())
-arr = {1: [0,6],2:[1,2],3:[0,2,3]}
-
-srch = [2,6]
-sol = srch * 0
-aa = {ID:max([time for time,IDs in arr.items() if ID in IDs]) for ID in srch}
-print(aa)
+#arr = {1: [0,6],2:[1,2],3:[0,2,3]}
+#
+#srch = [2,6]
+#sol = srch * 0
+#aa = {ID:max([time for time,IDs in arr.items() if ID in IDs]) for ID in srch}
+#print(aa)
 #a = list(filter(lambda x: x[1].index(srch) if srch in x[1]), arr.items()))
 #print(a)
 
-allFrozenLocalIDs0 = [[32,14], [28],[13]]
-allFrozenLocalIDs = sum(allFrozenLocalIDs0,[])
-print(f'drop {allFrozenLocalIDs}')
-temp = [[11,9,32,14],[4], [28,24],[0,13]]
-print(f'full {temp}')
-temp2 = []
-for subTemp in temp:
-    bfr = []
-    for elem in subTemp:
-        if elem not in allFrozenLocalIDs: bfr.append(elem)
-    temp2.append(bfr)
-#[for subTemp in temp2]
-print(temp2)#subTemp.remove(elem) for elem in allFrozenLocalIDs if allFrozenLocalIDs in subTemp 
+#allFrozenLocalIDs0 = [[32,14], [28],[13]]
+#allFrozenLocalIDs = sum(allFrozenLocalIDs0,[])
+#print(f'drop {allFrozenLocalIDs}')
+#temp = [[11,9,32,14],[4], [28,24],[0,13]]
+#print(f'full {temp}')
+#temp2 = []
+#for subTemp in temp:
+#    bfr = []
+#    for elem in subTemp:
+#        if elem not in allFrozenLocalIDs: bfr.append(elem)
+#    temp2.append(bfr)
+##[for subTemp in temp2]
+#print(temp2)#subTemp.remove(elem) for elem in allFrozenLocalIDs if allFrozenLocalIDs in subTemp 
+#
+#keysNewVals = np.array([24, 34, 24, 34, 31, 15, 24, 34, 39, 41, 34, 39], dtype=object)
+#dupWhereIndicies = {'5': [10, 11], '4': [6, 7, 8], 18: [0, 1], 28: [2, 3]}
+#keysOld = np.array([18, 18, 28, 28, '1', '0', '4', '4', '4', '6', '5', '5'], dtype=object)
+#dupSplit = ['5', '4', 18, 28]
+#dupWhereIndicies = {a:np.argwhere(keysOld == a) for a in dupSplit}
+#dupVals = {ID:keysNewVals[lst] for ID,lst in dupWhereIndicies.items()}
+import itertools, networkx as nx
+def overlappingRotatedRectangles(group1Params,group2Params):
+    group1IDs, group2IDs = list(group1Params.keys()),list(group2Params.keys())
+    allCombs = np.unique(np.sort(np.array(list(itertools.permutations(rectParams, 2)))), axis = 0)#;print(f'allCombs,{allCombs}')
+    intersectingCombs = []
+    for (keyOld,keyNew) in allCombs:
+        x1,y1,w1,h1 = group2Params[keyNew]
+        rotatedRectangle_new = ((x1+w1/2, y1+h1/2), (w1, h1), 0)
+        x2,y2,w2,h2 = group1Params[keyOld]
+        rotatedRectangle_old = ((x2+w2/2, y2+h2/2), (w2, h2), 0)
+        interType,_ = cv2.rotatedRectangleIntersection(rotatedRectangle_new, rotatedRectangle_old)
+        if interType > 0:
+            intersectingCombs.append([keyOld,keyNew]) # rough neighbors combinations
+    return intersectingCombs
 
-keysNewVals = np.array([24, 34, 24, 34, 31, 15, 24, 34, 39, 41, 34, 39], dtype=object)
-dupWhereIndicies = {'5': [10, 11], '4': [6, 7, 8], 18: [0, 1], 28: [2, 3]}
-keysOld = np.array([18, 18, 28, 28, '1', '0', '4', '4', '4', '6', '5', '5'], dtype=object)
-dupSplit = ['5', '4', 18, 28]
-dupWhereIndicies = {a:np.argwhere(keysOld == a) for a in dupSplit}
-dupVals = {ID:keysNewVals[lst] for ID,lst in dupWhereIndicies.items()}
+img = cv2.imread ('.\\manualMask\\frame0150.png',0)*0
+H,L = img.shape
+rectParams = {0:[0.15*L,0.4*H,200,200],1:[0.45*L,0.35*H,200,200],2:[0.75*L,0.35*H,200,200],3:[0.55*L,0.73*H,200,100],4:[0.7*L,0.60*H,100,100],5:[0.45*L,0.26*H,100,70],6:[0.52*L,0.17*H,100,70],7:[0.48*L,0.63*H,100,70],8:[0.18*L,0.65*H,100,70],9:[0.25*L,0.75*H,100,70]}
+rectParams = {i:list(map(int,a)) for i,a in rectParams.items()}; print(rectParams)
+rectParamsVert = {ID:[x,0,w,H] for ID,[x,y,w,h] in rectParams.items()}
+[cv2.rectangle(img, (x,y), (x+w,y+h), 128, -1) for [x,y,w,h] in rectParamsVert.values()]
+[cv2.rectangle(img, (x,y), (x+w,y+h), 255, -1) for [x,y,w,h] in rectParams.values()]
+#cv2.imshow('a', img)
+
+#aa = np.unique(np.sort(np.array(list(itertools.permutations(rectParams, 2)))), axis = 0)
+#print(aa)
+aa = overlappingRotatedRectangles(rectParamsVert,rectParamsVert);print(aa)
+HG = nx.Graph()
+HG.add_nodes_from(rectParams.keys())
+HG.add_edges_from(aa)
+cntrd = {ID:tuple((x+int(0.5*w),y+int(0.5*h))) for ID,[x,y,w,h] in rectParams.items()}
+# ----- visualize  netrworkx graph with background contrours
+
+    
+posTB = {ID:0 if y < (H - (y+h)) else 1 for ID,[x,y,w,h] in rectParams.items()};print(f'posTB:{posTB}')
+k = cv2.waitKey(0)
+if k == 27:  # close on ESC key
+    cv2.destroyAllWindows()
+q = 1
+neighbors = {ID:[] for ID in rectParams}
+neighborsSameSide = {ID:[] for ID in rectParams}
+for IDs in rectParams:
+    for i in HG.neighbors(IDs):
+        neighbors[IDs].append(i)
+        if posTB[i] == posTB[IDs]: neighborsSameSide[IDs].append(i)
+print(neighbors)
+print(neighborsSameSide)
+clusters0 = [tuple(np.sort(np.array([ID] +nbrs))) for ID, nbrs in neighborsSameSide.items()];print(clusters0)
+SameSideClusters = [list(aa) for aa in set(clusters0)]
+print(f'SameSideCluster:{SameSideClusters}')
+#print(np.argsort(list(hehe.values())))
+globSideOrder = {ID:0 for ID in rectParams}
+print(f'globSideOrder   :{globSideOrder}')
+for arr in SameSideClusters:
+    srt = np.argsort([cntrd[ID][1] for ID in arr]);print(srt)
+    for i,elem in enumerate(arr):
+        globSideOrder[elem] = srt[i]
+print(f'globSideOrder   :{globSideOrder}')
+print(f'posTB:{posTB}')
+
+textHeight  = 25
+textNoGo    = 10
+textSep     = 15
+q = 0
+#cv2.rectangle(img, (x,y), (x+w,y+h), 255, -1)
+for ID,[x,y,w,h] in rectParams.items():
+    if posTB[ID] == 1:
+        start = H - textNoGo - globSideOrder[ID]*(textHeight + textSep)
+        stop = start - textHeight
+        cv2.rectangle(img, (x,start), (x+w,stop), 255, 5) 
+    if posTB[ID] == 0:
+        start = textNoGo + globSideOrder[ID]*(textHeight + textSep)
+        stop = start + textHeight
+        cv2.rectangle(img, (x,start), (x+w,stop), 255, 5)
+
+if 1 == 1: 
+    #pos = {i:getCentroidPos(inp = vec, offset = (0,0), mode=0, mask=[]) for i, vec in cntrRemaining.items()}
+    for n, p in cntrd.items():
+            HG.nodes[n]['pos'] = p
+    plt.figure(1)
+    plt.imshow(img)
+    #for cntr in list(cntrRemaining.values()):
+    #    [x,y] = np.array(cntr).reshape(-1,2).T
+    #    plt.plot(x,y)
+    #[cv2.rectangle(img, (x,y), (x+w,y+h), 255, -1) for [x,y,w,h] in rectParams.values()]
+    nx.draw(HG, cntrd, with_labels=True)
+plt.show()
